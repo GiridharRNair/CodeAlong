35259a7d4877fff15fb3424f1fdf0476d98ca737
Giridhar Nair <Giridhar.r.nair@gmail.com>
2026-07-16 10:59:39 -0500

    Add "OLE" status to ExecuteResponse type for enhanced execution status tracking

--- models/execute_response.ts
+++ models/execute_response.ts
@@ -1,5 +1,5 @@
 type ExecuteResponse = {
-    status: "OK" | "TLE" | "MLE" | "RE" | "CE";
+    status: "OK" | "TLE" | "MLE" | "RE" | "CE" | "OLE";
     stdout: string;
     stderr: string;
     time: string;
